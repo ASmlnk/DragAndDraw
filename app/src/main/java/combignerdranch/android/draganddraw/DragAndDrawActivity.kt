94f9cc855f1885bb005a787fb4388c5e663677a7
user <sdelanss@gmail.com>
2023-07-04 10:26:12 +0300

    git pull origin master --allow-unrelated-histories

---
+++ app/src/main/java/combignerdranch/android/draganddraw/DragAndDrawActivity.kt
@@ -0,0 +1,11 @@
+package combignerdranch.android.draganddraw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DragAndDrawActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
